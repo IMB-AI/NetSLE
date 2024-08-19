0541bd7305b71773289b97c9ee85af04798cad2c
IMB-AI <95862137+IMB-AI@users.noreply.github.com>
2024-08-19 14:48:24 +0900

    Add files via upload

---
+++ code/pagerank.py
@@ -0,0 +1,82 @@
+import networkx as nx
+import pandas as pd
+import numpy as np
+import os
+import argparse
+
+### Construct network
+graph = nx.Graph()
+link = pd.read_csv(os.path.join('../data','network','string_0.txt'), sep = '\t', header = None)
+graph.add_edges_from(np.array(link))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--knowledge_path', required = False, default = '../data/SLE_prior_knowledge')
+args = parser.parse_args()
+
+prefix = args.knowledge_path.split('/')[-1]
+os.makedirs(os.path.join('../result', 'pagerank_score', prefix), exist_ok = True)
+
+if(args.knowledge_path == '../data/SLE_prior_knowledge'):
+	print('### BAFF (TNFSF13B) pagerank')
+	baff = {'TNFSF13B' : 1.0}
+
+	baff_score = nx.pagerank_scipy(graph, personalization = baff, max_iter = 10000, alpha = 0.5)
+	baff_score = pd.DataFrame(data = {'geneSymbol' : baff_score.keys(), 'pagerank_score' : baff_score.values()})
+
+	baff_score.sort_values(by = 'pagerank_score', ascending = False).to_csv(os.path.join('../result', 'pagerank_score', prefix, 'Drug_target.tsv'), sep = '\t', index = False)
+
+
+	print('### GWAS pagerank')
+	sle_gwas = pd.read_csv(os.path.join(args.knowledge_path, 'SLE_GWAS.txt'), sep = '\t', header = None)
+	gwas = dict()
+	for i,j in zip(sle_gwas[0].tolist(), sle_gwas[1].tolist()):
+		gwas[i] = j
+
+	gwas_score = nx.pagerank_scipy(graph, personalization = gwas, max_iter = 10000, alpha = 0.5)
+	gwas_score = pd.DataFrame(data = {'geneSymbol' : gwas_score.keys(), 'pagerank_score' : gwas_score.values()})
+
+	gwas_score.sort_values(by = 'pagerank_score', ascending = False).to_csv(os.path.join('../result', 'pagerank_score', prefix, 'GWAS.tsv'), sep = '\t', index = False)
+
+
+	print('### GC (Germinal center) pagerank')
+	gc_ = pd.read_csv(os.path.join(args.knowledge_path, 'GCall_genelist.txt'), sep = '\t', header = None)
+	gc = dict()
+	for i in gc_[0].tolist():
+		gc[i] = 1.0
+
+	gc_score = nx.pagerank_scipy(graph, personalization = gc, max_iter = 10000, alpha = 0.5)
+	gc_score = pd.DataFrame(data = {'geneSymbol' : gc_score.keys(), 'pagerank_score' : gc_score.values()})
+
+	gc_score.sort_values(by = 'pagerank_score', ascending = False).to_csv(os.path.join('../result', 'pagerank_score', prefix, 'GC.tsv'), sep = '\t', index = False)
+
+
+	print('### Cytokines (ImmuneXpresso) pagerank')
+	IX = pd.read_csv(os.path.join(args.knowledge_path, 'Systemic_lupus_erythematosus.csv'))
+	ix = dict()
+	gene_list = IX.drop_duplicates([' Cytokine Ontology Label'])[' Cytokine Ontology Label'].tolist()
+	for gene in gene_list:
+		ix[gene] = 1.0
+
+	ix_score = nx.pagerank_scipy(graph, personalization = ix, max_iter = 10000, alpha = 0.5)
+	ix_score = pd.DataFrame(data = {'geneSymbol' : ix_score.keys(), 'pagerank_score' : ix_score.values()})
+
+	ix_score.sort_values(by = 'pagerank_score', ascending = False).to_csv(os.path.join('../result', 'pagerank_score', prefix, 'IX.tsv'), sep = '\t', index = False)
+
+else:
+	knowledge_list = os.listdir(args.knowledge_path)
+	for file in knowledge_list:
+		print('###', file, sep = ' ')
+		temp = pd.read_csv(os.path.join(args.knowledge_path, file), sep = '\t',  header = None)
+		temp_dict = dict()
+		if(len(temp.columns) == 1):
+			for i in temp[0].tolist():
+				temp_dict[i] = 1.0
+
+		else:
+			for i,j in zip(temp[0].tolist(), temp[1].tolist()):
+				temp_dict[i] = j
+
+		temp_score = nx.pagerank_scipy(graph, personalization = temp_dict, max_iter = 10000, alpha = 0.5)
+		temp_score = pd.DataFrame(data = {'geneSymbol' : temp_score.keys(), 'pagerank_score' : temp_score.values()})
+
+		temp_score.sort_values(by = 'pagerank_score', ascending = False).to_csv(os.path.join('../result', 'pagerank_score', prefix, file.split('.')[0] + '.tsv'), sep = '\t', index = False)
---
+++ code/run_ml.py
@@ -0,0 +1,98 @@
+import pandas as pd
+from sklearn.ensemble import *
+from sklearn.model_selection import *
+from sklearn.metrics import *
+import numpy as np
+import copy as cp
+import os, math
+from tqdm import *
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--pagerank_path', required = False, default = '../result/pagerank_score/SLE_prior_knowledge')
+parser.add_argument('--DEG', required = False, default = '../data/DEG/SLE_DEGs.tsv')
+args = parser.parse_args()
+
+prefix = args.pagerank_path.split('/')[-1]
+os.makedirs(os.path.join('../result', 'disease_probability', prefix), exist_ok = True)
+
+if(args.pagerank_path == '../result/pagerank_score/SLE_prior_knowledge'):
+	### Load pagerank score
+	baff = pd.read_csv(os.path.join(args.pagerank_path, 'Drug_target.tsv'), sep = '\t')
+	gwas = pd.read_csv(os.path.join(args.pagerank_path, 'GWAS.tsv'), sep = '\t')
+	gc = pd.read_csv(os.path.join(args.pagerank_path, 'GC.tsv'), sep = '\t')
+	ix = pd.read_csv(os.path.join(args.pagerank_path, 'IX.tsv'), sep = '\t')
+
+	all_pagerank_score = cp.deepcopy(baff)
+	all_pagerank_score.rename(columns = {'pagerank_score' : 'BAFF'}, inplace = True)
+	all_pagerank_score = pd.merge(all_pagerank_score, gwas, on = 'geneSymbol')
+	all_pagerank_score.rename(columns = {'pagerank_score' : 'GWAS'}, inplace = True)
+	all_pagerank_score = pd.merge(all_pagerank_score, gc, on = 'geneSymbol')
+	all_pagerank_score.rename(columns = {'pagerank_score' : 'GC'}, inplace = True)
+	all_pagerank_score = pd.merge(all_pagerank_score, ix, on = 'geneSymbol')
+	all_pagerank_score.rename(columns = {'pagerank_score' : 'ImmuneXpresso'}, inplace = True)
+
+	feature_list = ['BAFF','GWAS','GC','ImmuneXpresso']
+	score_name = 'pSLE'
+	biomarker_name = 'NetSLE biomarker'
+
+	### Load differentially expressed genes (DEGs)
+	degs = pd.read_csv(args.DEG, sep = '\t')
+	deg_list = degs['geneSymbol'].tolist()
+else:
+	### Load pagerank score
+	file_list = os.listdir(args.pagerank_path)
+	print('Feature list: ', [x.split('.')[0] for x in file_list])
+	for i,file in enumerate(file_list):
+		temp = pd.read_csv(os.path.join(args.pagerank_path, file), sep = '\t')
+		temp.rename(columns = {'pagerank_score' : file.split('.')[0]}, inplace = True)
+		if(i == 0):
+			all_pagerank_score = cp.deepcopy(temp)
+		else:
+			all_pagerank_score = pd.merge(all_pagerank_score, temp, on = 'geneSymbol')
+
+	feature_list = [x.split('.')[0] for x in file_list]
+	score_name = 'pDisease'
+	biomarker_name = 'Disease biomarker'
+
+	### Load differntially expressed genes (DEGs)
+	degs = pd.read_csv(args.DEG, sep = '\t')
+	deg_list = degs.iloc[:,0].tolist()
+
+### Make table for machine learning 
+all_pagerank_score.loc[all_pagerank_score['geneSymbol'].isin(deg_list) == True, 'DEG'] = 1
+all_pagerank_score.loc[all_pagerank_score['DEG'].isnull() == True, 'DEG'] = 0
+all_pagerank_score['DEG'] = all_pagerank_score['DEG'].astype(int)
+
+
+### Construct machine learning classifier (random forest)
+rf = RandomForestClassifier(n_estimators = 1000, max_depth = 3)
+
+gene_list = []; proba_list = []; auroc_list = []; auprc_list = []
+for i in trange(1000):
+	X_train, X_test, y_train, y_test = train_test_split(all_pagerank_score.loc[:,['geneSymbol'] + feature_list], all_pagerank_score['DEG'], stratify = all_pagerank_score['DEG'], random_state = i, test_size = 0.1)
+	rf.fit(X_train.loc[:,feature_list], y_train.values.ravel())
+	y_proba = rf.predict_proba(X_test.loc[:,feature_list])
+	y_pred = rf.predict(X_test.loc[:,feature_list])
+
+	auroc_list.append(roc_auc_score(y_test, y_proba[:,1]))
+	precision, recall, threshold = precision_recall_curve(y_test, y_proba[:,1])
+	auprc_list.append(auc(recall, precision))
+
+	gene_list.extend(X_test['geneSymbol'].tolist())
+	proba_list.extend(list(y_proba[:,1]))
+
+df_perf = pd.DataFrame(data = {'Metric' : [x for x in ['AUROC','AUPRC'] for y in range(1000)], 'Performance' : auroc_list + auprc_list})
+
+print('Baseline of AUPRC: ', all_pagerank_score['DEG'].value_counts()[1] / len(all_pagerank_score))
+print(df_perf.groupby('Metric').mean('Performance'))
+
+df_proba = pd.DataFrame(data = {'geneSymbol' : gene_list, score_name : proba_list})
+df_proba_mean = df_proba.groupby('geneSymbol').mean(score_name)
+df_proba_mean.insert(0, 'geneSymbol', list(df_proba_mean.index))
+df_proba_mean = df_proba_mean.reset_index(drop = True)
+
+DF = pd.merge(df_proba_mean, all_pagerank_score.loc[:,['geneSymbol','DEG']], on = 'geneSymbol', how = 'left')
+DF = DF.sort_values(by = score_name, ascending = False).reset_index(drop = True)
+DF.to_excel(os.path.join('../result', 'disease_probability', prefix, biomarker_name + '.xlsx'), index = False)
